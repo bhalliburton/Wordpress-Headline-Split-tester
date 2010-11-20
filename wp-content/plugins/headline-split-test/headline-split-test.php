3a08e906ba8626a040f7cb816f51d8eaf6051275
Peter Bessman <peter@peterbessman.com>
2010-11-20 13:53:39 -0500

    counters increment correctly now

--- wp-content/plugins/headline-split-test/headline-split-test.php
+++ wp-content/plugins/headline-split-test/headline-split-test.php
@@ -28,7 +28,10 @@ License: GPL2
 class HEADST4WP {
 
 	var $meta = 'headst4wp';
-	var $title_map = array();
+	var $title_map = array();				// keeps rendered titles and urls in sync
+	var $impressions_count_map = array();	// ensures that impressions only get incremented once
+	var $alt_clicks_map = array();			// ensures that alt titles only get incremented once
+	var $pri_clicks_map = array();			// ensures that primary titles only get incremented once
     
 	function HEADST4WP ($meta = 'headst4wp', $control_in_head = true) {
 		$this->__construct($meta, $control_in_head);
@@ -52,17 +55,20 @@ class HEADST4WP {
 			add_filter('post_link', array(&$this, 'link_filter'), 1, 2);
 		}
 		register_activation_hook(__FILE__,'set_headline_split_options');
-    register_deactivation_hook(__FILE__,'unset_headline_split_options');
-    add_action('admin_menu',array(&$this,'modify_menu'));
+	    register_deactivation_hook(__FILE__,'unset_headline_split_options');
+	    add_action('admin_menu',array(&$this,'modify_menu'));
 	}
+	
 	
 	function set_headline_split_options() {
 	  add_option('headline_split_impressions','250','Number of impressions to show before deciding');
 	}
 	
+	
 	function unset_headline_split_options() {
 	  delete_option('headline_split_impressions');
 	}	
+	
 	
 	function update_headline_split_options() {
 	  $ok = false;
@@ -83,30 +89,34 @@ class HEADST4WP {
 	  }
 	}
 	
+	
 	function print_headline_split_form() {
-	  $default_impressions = get_option('headline_split_impressions');
-	  ?>
-	  <form method="post">
-	    <label for="headline_split_impressions">Number of Impressions to Show Before Deciding:
-	      <input type="text" name="headline_split_impressions" value="<?=$default_impressions?>" />
-	    </label>
-	  </form>
-	  <?php
+  		$default_impressions = get_option('headline_split_impressions');
+?>
+<form method="post">
+	<label for="headline_split_impressions">Number of Impressions to Show Before Deciding:</label>
+    <input type="text" name="headline_split_impressions" value="<?=$default_impressions?>" />
+	<input type="submit" name="submit" value="Submit" />
+</form>
+<?php
 	}
+	
 	
 	function admin_headline_split_options() {
 	  ?><div class="wrap"><h2>Headline Split Tester Options</h2><?php
 	  
-	  if($_REQUEST['submit']) {
+	  if(isset($_REQUEST['submit'])) {
 	    $this->update_headline_split_options();
 	  }
 	  $this->print_headline_split_form();
 	  
 	}
 	
+
 	function modify_menu() {
 	  add_options_page('Headline Split Tester','Headline Split Tester','manage_options',__FILE__,array(&$this,'admin_headline_split_options'));
 	}
+	
 	
 	function get_alt_headline($id){
 		$options = get_post_meta($id, $this->meta, true);
@@ -121,49 +131,58 @@ class HEADST4WP {
 	}
 	
 	
-	function get_headline_clicks($id, $is_alt) {
-		$options = get_post_meta($id, $this->meta, true);
-		$index = 'pri_headline_clicks';
-		
-		if ($is_alt)
-			$index = 'alt_headline_clicks';
-			
-		return (int) isset($options[$index]) ? $options[$index] : 0;
-	}
-	
-	
-	function get_headline_impressions($id) {
-		$options = get_post_meta($id, $this->meta, true);
-		
-		return (int) isset($options['headline_impressions']) ? $options['headline_impressions'] : 0;
-	}
-	
-	
 	function increment_headline_clicks($id, $is_alt) {
-		$clicks = $this->get_headline_clicks($id, $is_alt);
-		$clicks++;
-		
-		$index = 'pri_headline_clicks';
-		if ($is_alt)
-			$index = 'alt_headline_clicks';
+		if ($is_alt && array_key_exists($id, $this->alt_clicks_map))
+			return;
 			
-		$options = array();
+		if (array_key_exists($id, $this->pri_clicks_map))
+			return;
+			
+		$options = get_post_meta($id, $this->meta, true);
+		$clicks = 0;
+		$index = $is_alt? 'alt_headline_clicks': 'pri_headline_clicks';
+		
+		if (is_array($options)) {
+			if (isset($options[$index]))
+				$clicks = (int) $options[$index];
+		} else {
+			$options = array();
+		}
+
+		$clicks++;
 		$options[$index] = $clicks;
 		
 		if (!update_post_meta($id, $this->meta, $options))
 			add_post_meta($id, $this->meta, $options);
+		
+		if ($is_alt)
+			$this->alt_clicks_map[$id] = $id;
+		else
+			$this->pri_clicks_map[$id] = $id;
 	}
 	
 	
 	function increment_headline_impressions($id) {
-		$impressions = $this->get_headline_impressions($id);
-		$impressions++;
+		if (array_key_exists($id, $this->impressions_count_map))
+			return;
+			
+		$options = get_post_meta($id, $this->meta, true);
+		$impressions = 0;
 		
-		$options = array();
+		if (is_array($options)) {
+			 if (isset($options['headline_impressions']))
+				$impressions = (int) $options['headline_impressions'];
+		} else {
+			$options = array();
+		}
+		
+		$impressions++;
 		$options['headline_impressions'] = $impressions;
 		
 		if (!update_post_meta($id, $this->meta, $options))
 			add_post_meta($id, $this->meta, $options);
+			
+		$this->impressions_count_map[$id] = $id;
 	}
 	
 	
