febee6c8e6f8c1a4a2e9d285b7e5e1217ef6da1a
bhalliburton <brent_halliburton@yahoo.com>
2010-11-19 20:26:19 -0500

    some code

---
+++ wp-content/plugins/headline-split-test/headline-split-test.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Headline Split Test
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 0.1
+Author: Brent Halliburton & Peter Bessman
+Author URI: http://URI_Of_The_Plugin_Author
+License: GPL2
+*/
+
+/*  Copyright 2010 Brent Halliburton & Peter Bessman  (email : headlinesplittest@bhalliburton.otherinbox.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+?>
